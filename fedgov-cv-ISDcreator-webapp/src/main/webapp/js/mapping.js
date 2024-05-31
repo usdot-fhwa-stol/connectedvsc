18631ac90068d09be6a5966bb25fd0acaf36b535
jwillmartin <jw.martin@outlook.com>
2024-05-31 10:00:34 -0400

    modified:   fedgov-cv-ISDcreator-webapp/src/main/webapp/js/mapping.js 	modified:   fedgov-cv-TIMcreator-webapp/src/main/webapp/js/mapping.js

--- fedgov-cv-ISDcreator-webapp/src/main/webapp/js/mapping.js
+++ fedgov-cv-ISDcreator-webapp/src/main/webapp/js/mapping.js
@@ -6,43 +6,44 @@
 /**
  * DEFINE GLOBAL VARIABLES
  */
-    var map;
-    var vectors, lanes, laneMarkers, box, laneConnections, errors, trace, laneWidths;
-    var fromProjection, toProjection;
-    var temp_lat, temp_lon, selected_marker, selected_layer;
-    var intersection_url = '//api.geonames.org/findNearestIntersectionJSON';
-    var elevation_url = 'https://dev.virtualearth.net/REST/v1/Elevation/List?hts=ellipsoid&points=';
-    var computingLane = false;
-    var computedLaneSource;
-    var sharedWith_object = '';
-    var typeAttribute_object = '';
-    var typeAttributeName = '';
-    var typeAttributeNameSaved = '';
-    var sharedWith = [];
-    var typeAttribute = [];
-    var laneTypeOptions = [];
-    var nodeLaneWidth = [];
-    var signalPhase, stateConfidence, laneNum, laneType, approachType, intersectionID, approachID, regionID;
-    var nodeObject = [];
-    var revisionNum = 0;
-	let cachedApiKey = null;
-	let cachedUsername = null;
 
-    var bingResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677, 0.14929107084870338,
-        0.07464553542435169];
-    var bingServerResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677];
+var map;
+var vectors, lanes, laneMarkers, box, laneConnections, errors, trace, laneWidths;
+var fromProjection, toProjection;
+var temp_lat, temp_lon, selected_marker, selected_layer;
+var intersection_url = '//api.geonames.org/findNearestIntersectionJSON';
+var elevation_url = 'https://dev.virtualearth.net/REST/v1/Elevation/List?hts=ellipsoid&points=';
+var computingLane = false;
+var computedLaneSource;
+var sharedWith_object = '';
+var typeAttribute_object = '';
+var typeAttributeName = '';
+var typeAttributeNameSaved = '';
+var sharedWith = [];
+var typeAttribute = [];
+var laneTypeOptions = [];
+var nodeLaneWidth = [];
+var signalPhase, stateConfidence, laneNum, laneType, approachType, intersectionID, approachID, regionID;
+var nodeObject = [];
+var revisionNum = 0;
+let cachedApiKey = null;
+let cachedUsername = null;
+
+var bingResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
+	19567.87923828125, 9783.939619140625, 4891.9698095703125,
+	2445.9849047851562, 1222.9924523925781, 611.4962261962891,
+	305.74811309814453, 152.87405654907226, 76.43702827453613,
+	38.218514137268066, 19.109257068634033, 9.554628534317017,
+	4.777314267158508, 2.388657133579254, 1.194328566789627,
+	0.5971642833948135, 0.29858214169740677, 0.14929107084870338,
+	0.07464553542435169];
+var bingServerResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
+	19567.87923828125, 9783.939619140625, 4891.9698095703125,
+	2445.9849047851562, 1222.9924523925781, 611.4962261962891,
+	305.74811309814453, 152.87405654907226, 76.43702827453613,
+	38.218514137268066, 19.109257068634033, 9.554628534317017,
+	4.777314267158508, 2.388657133579254, 1.194328566789627,
+	0.5971642833948135, 0.29858214169740677];
 
 async function getApiKey() {
 	if (cachedApiKey) {
--- fedgov-cv-TIMcreator-webapp/src/main/webapp/js/mapping.js
+++ fedgov-cv-TIMcreator-webapp/src/main/webapp/js/mapping.js
@@ -8,35 +8,35 @@
  * DEFINE GLOBAL VARIABLES
  */
 
-    var map;
-    var vectors, lanes, laneMarkers, area, polygons, polyMarkers, radiuslayer, trace, laneWidths;
-    var fromProjection, toProjection;
-    var temp_lat, temp_lon, selected_marker, selected_layer, selected_marker_limit;
-    var mutcd, priority, direction, extent, info_type, ttl;
-    var circle_bounds;
+var map;
+var vectors, lanes, laneMarkers, area, polygons, polyMarkers, radiuslayer, trace, laneWidths;
+var fromProjection, toProjection;
+var temp_lat, temp_lon, selected_marker, selected_layer, selected_marker_limit;
+var mutcd, priority, direction, extent, info_type, ttl;
+var circle_bounds;
 
-    var content = [];
-    var elevation_url = 'https://dev.virtualearth.net/REST/v1/Elevation/List?hts=ellipsoid&points=';
-    var nodeLaneWidth = [];
-    var circles_temp = [];
-    var circles_reset = [];
-    let cachedApiKey = null;
+var content = [];
+var elevation_url = 'https://dev.virtualearth.net/REST/v1/Elevation/List?hts=ellipsoid&points=';
+var nodeLaneWidth = [];
+var circles_temp = [];
+var circles_reset = [];
+let cachedApiKey = null;
 
-    var bingResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677, 0.14929107084870338,
-        0.07464553542435169];
-     var bingServerResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677];
+var bingResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
+    19567.87923828125, 9783.939619140625, 4891.9698095703125,
+    2445.9849047851562, 1222.9924523925781, 611.4962261962891,
+    305.74811309814453, 152.87405654907226, 76.43702827453613,
+    38.218514137268066, 19.109257068634033, 9.554628534317017,
+    4.777314267158508, 2.388657133579254, 1.194328566789627,
+    0.5971642833948135, 0.29858214169740677, 0.14929107084870338,
+    0.07464553542435169];
+    var bingServerResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
+    19567.87923828125, 9783.939619140625, 4891.9698095703125,
+    2445.9849047851562, 1222.9924523925781, 611.4962261962891,
+    305.74811309814453, 152.87405654907226, 76.43702827453613,
+    38.218514137268066, 19.109257068634033, 9.554628534317017,
+    4.777314267158508, 2.388657133579254, 1.194328566789627,
+    0.5971642833948135, 0.29858214169740677];
 
 async function getApiKey() {
     if (cachedApiKey) {
