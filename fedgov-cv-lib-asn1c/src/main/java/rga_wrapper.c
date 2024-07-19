5378463e8847761924f2ba13ddf87bee063fa2bb
DokurOmkar <omkar.dokur@leidos.com>
2024-07-19 11:31:54 -0700

    MAP 144 - Added the files for JNI wrapper and generated .so file

---
+++ fedgov-cv-lib-asn1c/src/main/java/rga_wrapper.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2024 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include "gov_usdot_cv_rgaencoder_Encoder.h"
+#include "MessageFrame.h"
+#include <stdint.h>
+
+JNIEXPORT jbyteArray JNICALL Java_gov_usdot_cv_rgaencoder_Encoder_encodeRGA(JNIEnv *env, jint majorVer, jint minorVer)
+{
+    printf("\n ***Inside the rga_wrapper.c file **** \n");
+	uint8_t buffer[2302];
+	size_t buffer_size = sizeof(buffer);
+	asn_enc_rval_t ec;
+
+    MessageFrame_t *message;
+
+	message = calloc(1, sizeof(MessageFrame_t));
+	if (!message)
+	{
+		return NULL;
+	}
+
+	// set default value of messageId of DSRCmsgID_roadGeometryAndAttributes 
+	message->messageId = 43;
+
+    // set default Message Frame Value for RoadGeometryAndAttributes
+	message->value.present = MessageFrame__value_PR_RoadGeometryAndAttributes;
+
+
+    ec = uper_encode_to_buffer(&asn_DEF_MessageFrame, 0, message, buffer, buffer_size);
+	if (ec.encoded == -1)
+	{
+		// printf("Cause of failure %s \n", ec.failed_type->name)
+		printf("Unsuccessful!\n");
+		return NULL;
+	}
+	printf("Successful!\n");
+
+	jsize length = ec.encoded / 8;
+	jbyteArray outJNIArray = (*env)->NewByteArray(env, length);
+	if (outJNIArray == NULL)
+	{
+		return NULL;
+	}
+
+	(*env)->SetByteArrayRegion(env, outJNIArray, 0, length, buffer);
+
+	free(message);
+
+	// Get a pointer to the array elements
+	jbyte *elements = (*env)->GetByteArrayElements(env, outJNIArray, NULL);
+
+	if (elements == NULL)
+	{
+		return; // Error handling, if necessary
+	}
+
+	// Loop through and print each element
+	for (jsize i = 0; i < length; i++)
+	{
+		printf("Element %d: %d\n", i, elements[i]);
+	}
+	(*env)->ReleaseByteArrayElements(env, outJNIArray, elements, JNI_ABORT);
+
+	return outJNIArray;
+}
