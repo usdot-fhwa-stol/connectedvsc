ec8e4492b00431994e7679b3028ba67d3fff2645
dan-du-car <dan.du@leidos.com>
2024-09-04 16:40:54 -0400

    revert bing.js

--- fedgov-cv-ISDcreator-webapp/src/main/webapp/js/bing.js
+++ fedgov-cv-ISDcreator-webapp/src/main/webapp/js/bing.js
@@ -1,402 +1,104 @@
 /**
- * Created by lewisstet on 3/20/2015.
+ * Created by martzth on 2/15/2017.
  * Updated 3/2017 by martzth
  */
 
+
 /**
- * @defines: intersection features, help notes, lane attributes
+ * Purpose: to lookup addresses using the bing API
+ * @params: input text -> address, city, and state
+ * @event: sets lat/long cookie and moves map
+ *
+ * @deprecated for google geocomplete in main.js
  */
 
-var intersection_features = [
-    {
-        id: 0,
-        name: "Reference Point Marker",
-        description: "Used to mark the center of the intersection",
-        img_src: "./img/intersection-builder/markers/Map-Marker-Marker-Outside-Azure-icon.png"
-    },
-    {
-        id: 1,
-        name: "Verified Point Marker",
-        description: "Used to mark a known, verified location near an intersection",
-        img_src: "./img/intersection-builder/markers/Map-Marker-Ball-Azure-icon.png"
+/*
+ $('#address-search-btn').click(function () {
+
+ var str = $('#address-search').val();
+ var address = str.split(',');
+ var street = address[0];
+ var city = address[1];
+ var state = address[2];
+ $('#address-search').val('');
+
+ $.ajax({
+ url: "https://dev.virtualearth.net/REST/v1/Locations/US/" + state + "/" + city + "/" + street + "?&key=" + apiKey,
+ dataType: "jsonp",
+ jsonp: "jsonp",
+ success: function (result) {
+
+ var search_lat = result.resourceSets[0].resources[0].point.coordinates[0];
+ var search_lon = result.resourceSets[0].resources[0].point.coordinates[1];
+
+ setCookie("tim_latitude", search_lat, 365);
+ setCookie("tim_longitude", search_lon, 365);
+
+ try {
+ var location = new OpenLayers.LonLat(search_lon, search_lat);
+ location.transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+ map.setCenter(location, 18);
+ }
+ catch (err) {
+ console.log("No vectors to reset view");
+ }
+ },
+ error: function (error){
+ console.log("Location error: ", error);
+ }
+ });
+
+ });
+ */
+
+
+/**
+ * Purpose: to find and display tile age
+ * @params: lat/long -> on map move (event registered in mapping.js)
+ * @event: sets tile age text on DOM
+ */
+
+async function tileAge() {
+    const apiKey = await getApiKey();
+
+    var convertedLonLat = new OpenLayers.LonLat(map.getCenter().lon, map.getCenter().lat).transform(toProjection, fromProjection);
+    var current_zoom = map.getZoom();
+    if (current_zoom > 18) {
+        current_zoom = 18;
     }
-];
+    $.ajax({
+        url: "https://dev.virtualearth.net/REST/v1/Imagery/Metadata/Aerial/" + convertedLonLat.lat + "," + convertedLonLat.lon + "?uriScheme=https&zl=" + current_zoom + "&key=" + apiKey,
+        dataType: "jsonp",
+        jsonp: "jsonp",
+        success: function (result) {
+            try {
+                var start = (result.resourceSets[0].resources[0].vintageStart).split(" ");
+                var end = (result.resourceSets[0].resources[0].vintageEnd).split(" ");
+                $('#tileAge .start').text(start[1] + "/" + start[2]);
+                $('#tileAge .end').text(end[1] + "/" + end[2]);
+            }
+            catch (err) {
+                $('#tileAge .start').text("");
+                $('#tileAge .end').text("");
+            }
+        },
+        error: function (error) {
+            console.log("Location error: ", error);
+        }
+    });
 
-var lane_attributes = [
-	{
-	  id: 0,
-	  name: "Straight",
-	  description: "Maneuver Straight Allowed",
-	  img_src: "./img/intersection-builder/lane/straight.png"
-	},
-	{
-	  id: 1,
-	  name: "Left",
-	  description: "Maneuver Left Allowed",
-	  img_src: "./img/intersection-builder/lane/left.png"
-	},
-	{
-	  id: 2,
-	  name: "Right",
-	  description: "Maneuver Right Allowed",
-	  img_src: "./img/intersection-builder/lane/right.png"
-	},
-	{
-	  id: 3,
-	  name: "U-Turn",
-	  description: "Maneuver U-Turn Allowed",
-	  img_src: "./img/intersection-builder/lane/u-turn.png"
-	},
-	{
-	  id: 4,
-	  name: "Left Turn on Red",
-	  description: "Maneuver Left Turn on Red Allowed",
-	  img_src: "./img/intersection-builder/lane/left-turn-on-red.jpg"
-	},
-	{
-	  id: 5,
-	  name: "Right Turn on Red",
-	  description: "Maneuver Right Turn on Red Allowed",
-	  img_src: "./img/intersection-builder/lane/right-turn-on-red.jpg"
-	},
-	{
-	  id: 6,
-	  name: "Lange Change",
-	  description: "Lane Change Allowed",
-	  img_src: "./img/intersection-builder/lane/lane-change.png"
-	},
-	{
-	  id: 7,
-	  name: "No Stopping",
-	  description: "No Stopping Allowed",
-	  img_src: "./img/intersection-builder/lane/no-stopping.gif"
-	},
-	{
-	  id: 8,
-	  name: "Yield Always Required",
-	  description: "Yield Always Required",
-	  img_src: "./img/intersection-builder/lane/yield.png"
-	},
-	{
-	  id: 9,
-	  name: "Go With Halt",
-	  description: "Go With Halt",
-	  img_src: "./img/intersection-builder/lane/stop-sign.png"
-	},
-	{
-	  id: 10,
-	  name: "Caution",
-	  description: "Caution",
-	  img_src: "./img/intersection-builder/lane/caution.png"
-	}
-];
+}
 
-var help_notes = [
-    {
-    	value: "approach_type",
-    	title: "Approach Type",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies the type of the approach."
-    },
-    {
-    	value: "direction",
-    	title: "Direction",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Denotes the allowed direction of travel over a lane object."
-    },
-    {
-    	value: "descriptive_name",
-    	title: "Descrpitive Name",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies a short descriptive name for the lane. Max length is 63 characters."
-    },
-    {
-    	value: "lane_type",
-    	title: "Lane Type",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies the type of the lane."
-    },
-    {
-    	value: "lane_type_attributes",
-    	title: "Lane Type Attributes",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Attributes information specific to a given lane type."
-    },
-    {
-    	value: "shared_with",
-    	title: "Shared With",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Describes co-users of the lane path."
-    },
-    {
-    	value: "lane_number",
-    	title: "Lane Number",
-    	max: "255",
-    	min: "0",
-    	units: "N/A",
-    	description: "Conveys an assigned index that is unique within an intersection. <br> --The value 0 shall be used" +
-    			"when the lane ID is not available or not known. <br> --The value 255 is reserved for future use. "
-    },
-    {
-    	value: "approach_id",
-    	title: "Approach ID",
-    	max: "15",
-    	min: "0",
-    	units: "N/A",
-    	description: "Used to relate the index of an approach, either ingress or egress within the subject lane. <br>" +
-    			"--Zero to be used when valid value is unknown."
-    },
-    {
-    	value: "intersection_name",
-    	title: "Intersection Name",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies the name of the intersection."
-    },
-    {
-    	value: "layer_id",
-    	title: "Layer ID",
-    	max: "100",
-    	min: "0",
-    	units: "N/A",
-    	description: "Used to uniquely identify the layers of a geographic map fragment such as an intersection."
-    },
-    {
-    	value: "revision",
-    	title: "Revision Number",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies revision number."
-    },
-    {
-    	value: "latitude",
-    	title: "Latitude",
-    	max: "90.0",
-    	min: "-90.0",
-    	units: "Decimal Degrees",
-    	description: "The geographic latitude of an object."
-    },
-    {
-    	value: "longitude",
-    	title: "Longitude",
-    	max: "180.0",
-    	min: "-180.0",
-    	units: "Decimal Degrees",
-    	description: "The geographic longitude of an object."
-    },
-	{
-    	value: "region_id",
-    	title: "Region ID",
-    	max: "65535",
-    	min: "0",
-    	units: "N/A",
-    	description: 'A globally unique regional assignment value that is typically assigned to a regional DOT authority. Also known as "Road Regulator ID" and is a portion of the "Intersection Reference ID".'
-    },
-    {
-    	value: "intersection_id",
-    	title: "Intersection ID",
-    	max: "65535",
-    	min: "0",
-    	units: "N/A",
-    	description: "Used within a region to uniquely define an intersection within that country or region."
-    },
-    {
-    	value: "elevation",
-    	title: "Elevation",
-    	max: "6143.9",
-    	min: "-409.6",
-    	units: "Meters",
-    	description: "The geographic elevation of an object."
-    },
-    {
-    	value: "lane_width",
-    	title: "Lane Width",
-    	max: "511",
-    	min: "-512",
-    	units: "Centimeters",
-    	description: "Used to convey the delta width of a lane in LSB units of 1cm."
-    },
-    {
-    	value: "master_lane_width",
-    	title: "Master Lane Width",
-    	max: "32767",
-    	min: "0",
-    	units: "Centimeters",
-    	description: "An overriding value to set the width of all described lanes."
-    },
-    {
-    	value: "verified_lat",
-    	title: "Verified Latitude",
-    	max: "90.0",
-    	min: "-90.0",
-    	units: "Decimal Degrees",
-    	description: "The verified geographic latitude of an object."
-    },
-    {
-    	value: "verified_long",
-    	title: "Verified Longitude",
-    	max: "180.0",
-    	min: "-180.0",
-    	units: "Decimal Degrees",
-    	description: "The verified geographic longitude of an object."
-    },
-    {
-    	value: "verified_elev",
-    	title: "Verified Elevation",
-    	max: "6143.9",
-    	min: "-409.6",
-    	units: "Meters",
-    	description: "The verified geographic elevation of an object."
-    },
-    {
-    	value: "spat_revision",
-    	title: "SPaT Revision",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies SPaT revision."
-    },
-    {
-    	value: "spat_label",
-    	title: "Signal Group",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Specifies Signal Group ID."
-    },
-    {
-    	value: "phase",
-    	title: "Phase",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Provides the overall current state of the movement."
-    },
-    {
-    	value: "start_time",
-    	title: "Start Time",
-    	max: "65535",
-    	min: "1",
-    	units: "LSB units of 10 mSec",
-    	description: "Used to relate when the phase itself started or is expected to start."
-    },
-    {
-    	value: "min_end_time",
-    	title: "Minimum End Time",
-    	max: "65535",
-    	min: "1",
-    	units: "LSB units of 10 mSec",
-    	description: "Used to convey the earliest time possible at which a phase could change, except when unpredictable events relating to a preemption or priority call disrupt a currently active timing plan."
-    },
-    {
-    	value: "max_end_time",
-    	title: "Maximum End Time",
-    	max: "65535",
-    	min: "1",
-    	units: "LSB units of 10 mSec",
-    	description: "Used to convey the longest expected end time."
-    },
-    {
-    	value: "likely_time",
-    	title: "Likely Time",
-    	max: "65535",
-    	min: "1",
-    	units: "LSB units of 10 mSec",
-    	description: "Used to convey the most likely time the phase changes."
-    },
-    {
-    	value: "confidence",
-    	title: "Confidence",
-    	max: "100%",
-    	min: "21%",
-    	units: "Percentage",
-    	description: "Used to convey basic confidnece data about likely time."
-    },
-    {
-    	value: "next_time",
-    	title: "Next Time",
-    	max: "65535",
-    	min: "1",
-    	units: "LSB units of 10 mSec",
-    	description: "Used to express a general (and presumably less precise) value regarding when this phase will next occur."
-    },
-    {
-    	value: "speed_limit_type",
-    	title: "Speed Limit Type",
-    	max: "N/A",
-    	min: "N/A",
-    	units: "N/A",
-    	description: "Relates the type of speed limit to which a given speed refers."
-    },
-    {
-    	value: "velocity",
-    	title: "Velocity",
-    	max: "366",
-    	min: "0",
-    	units: "Miles Per Hour",
-    	description: "Represents the vehicle speed."
-    },
-	{
-		value: "referenceLaneID",
-		title: "Reference Lane ID",
-		max: "63",
-		min: "1",
-		units: "N/A",
-		description: "Represents a computed lanes reference lane."
-	},
-	{
-		value: "offsetX",
-		title: "Offset X",
-		max: "2047",
-		min: "-2047",
-		units: "Centimeters",
-		description: "A path X offset for translations."
-	},
-	{
-		value: "offsetY",
-		title: "Offset Y",
-		max: "2047",
-		min: "-2047",
-		units: "Centimeters",
-		description: "A path Y offset for translations."
-	},
-	{
-		value: "rotation",
-		title: "Rotation",
-		max: "359.9875",
-		min: "0",
-		units: "Degrees",
-		description: "A rotation value for the entire lane."
-	},
-	{
-		value: "scaleX",
-		title: "Scale X",
-		max: "102.35",
-		min: "-99.95",
-		units: "Steps of 0.05 percent",
-		description: "Expand or contract lane along the X axis."
-	},
-	{
-		value: "scaleY",
-		title: "Scale Y",
-		max: "102.35",
-		min: "-99.95",
-		units: "Steps of 0.05 percent",
-		description: "Expand or contract lane along the Y axis."
-	}
-    
-];
+
+/**
+ * Purpose: set cookie so map loads to same position
+ * @params: name, lat/lon or zoom, days of expiration (365)
+ * @event: sets cookie
+ */
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
