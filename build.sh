b4413a90e4ba720c4c7642392ae1d909aac81e3a
ChrisC027 <christopher.h.castillo@leidos.com>
2024-10-10 22:51:33 -0400

    revert build script

--- build.sh
+++ build.sh
@@ -1,29 +1,24 @@
-sudo rm -rf /home/vboxuser/connectedvcs-tools/fedgov-cv-mapencoder/logs/cv-mapencoder.log
-sudo rm -rf /home/vboxuser/connectedvcs-tools/fedgov-cv-mapencoder/applogs
-sudo rm -rf /home/vboxuser/connectedvcs-tools/fedgov-cv-message-builder/logs/cv-message-builder.log
-
-LD_LIBRARY_PATH="/home/vboxuser/connectedvcs-tools/fedgov-cv-lib-asn1c/third_party_lib"
-export LD_LIBRARY_PATH
 # Build Steps
 cd ./fedgov-cv-parent
-mvn clean install -U
+mvn install -DskipTests=true
 
 cd ../fedgov-cv-lib-asn1c/
-mvn clean install -U
+mvn install -DskipTests=true
 
 cd ../fedgov-cv-mapencoder/
-mvn clean install -U
+mvn install -DskipTests=true
 
 cd ../fedgov-cv-rgaencoder/
-mvn clean install -U
+mvn install -DskipTests=true
 
 cd ../fedgov-cv-message-builder/
-mvn clean install -U
+mvn install -DskipTests=true
 
-# cd ../fedgov-cv-ISDcreator-webapp/
-# mvn clean install -U
+cd ../fedgov-cv-ISDcreator-webapp/
+mvn install -DskipTests=true
 
-# cd ../fedgov-cv-TIMcreator-webapp/
-# mvn clean install -U
+cd ../fedgov-cv-TIMcreator-webapp/
+mvn install -DskipTests=true
 
+jar cvf ../private-resources.war -C ../private-resources .
 jar cvf ../root.war -C ../root .
