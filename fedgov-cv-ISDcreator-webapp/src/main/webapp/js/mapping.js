02b70482be93a375e5fb9abc5cee3e038f13ea1c
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-11-04 11:19:52 -0500

    Revert "Sync master(Hotfix 2.0.1) into develop  (#53)" (#54)
    This reverts commit 58b618e03757df30c531d4810468a10c1eee10cd.

--- fedgov-cv-ISDcreator-webapp/src/main/webapp/js/mapping.js
+++ fedgov-cv-ISDcreator-webapp/src/main/webapp/js/mapping.js
@@ -7,43 +7,6 @@
  * DEFINE GLOBAL VARIABLES
  */
 
-    var map;
-    var vectors, lanes, laneMarkers, box, laneConnections, errors, trace, laneWidths;
-    var fromProjection, toProjection;
-    var temp_lat, temp_lon, selected_marker, selected_layer;
-    var intersection_url = '//api.geonames.org/findNearestIntersectionJSON';
-	var google_elevation_url='/msp/googlemap/api/elevation';
-    var computingLane = false;
-    var computedLaneSource;
-    var sharedWith_object = '';
-    var typeAttribute_object = '';
-    var typeAttributeName = '';
-    var typeAttributeNameSaved = '';
-    var sharedWith = [];
-    var typeAttribute = [];
-    var laneTypeOptions = [];
-    var nodeLaneWidth = [];
-    var signalPhase, stateConfidence, laneNum, laneType, approachType, intersectionID, approachID;
-    var nodeObject = [];
-    var revisionNum = 0;
-
-    var bingResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677, 0.14929107084870338,
-        0.07464553542435169];
-    var bingServerResolutions = [156543.03390625, 78271.516953125, 39135.7584765625,
-        19567.87923828125, 9783.939619140625, 4891.9698095703125,
-        2445.9849047851562, 1222.9924523925781, 611.4962261962891,
-        305.74811309814453, 152.87405654907226, 76.43702827453613,
-        38.218514137268066, 19.109257068634033, 9.554628534317017,
-        4.777314267158508, 2.388657133579254, 1.194328566789627,
-        0.5971642833948135, 0.29858214169740677];
-
-
 var map;
 var vectors, lanes, laneMarkers, box, laneConnections, errors, trace, laneWidths;
 var fromProjection, toProjection;
@@ -2139,32 +2102,36 @@ async function populateRefWindow(feature, lat, lon)
             }
 		}
 	});
-	if(!feature.attributes.elevation)
-	{
-		var elev;
-		$.ajax({
-			url: google_elevation_url+"/"+lat+'/'+lon,
-			success: function(result){
-				console.log(result);
-				elev = result?.elevation;
-				// elev = result.resourceSets[0].resources[0].elevations[0];
-				if (elev == null|| elev==undefined){
-					elev = -9999; //any sea value is set to -9999 by default. This brings it back to sea level as we know it
-				}else{
-					elev = Math.round(elev);
-				}
-				if (! feature.attributes.elevation ) {
-					$('#elev').val(elev);
-				} else {
-					if (feature.attributes.number > -1) {
-						if (!feature.attributes.elevation.value) {
-							$('#elev').val(elev);
-						}
-					}
-				}
-			}
-		});
-	}
+
+	var elev;
+
+    $.ajax({
+        url: elevation_url + lat + ',' + lon + '&key=' + apiKey,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        cache: false,
+        success: function(result){
+            elev = result.resourceSets[0].resources[0].elevations[0];
+            if (elev == null){
+                elev = -9999; //any sea value is set to -9999 by default. This brings it back to sea level as we know it
+            }
+            if (! feature.attributes.elevation ) {
+                $('#elev').val(elev);
+            } else {
+                if (feature.attributes.number > -1) {
+                    if (!feature.attributes.elevation.value) {
+                        $('#elev').val(elev);
+                    }
+                }
+            }
+            if (feature.attributes.verifiedElev){
+                $('#verified_elev').val(feature.attributes.verifiedElev);
+            } else {
+                $('#verified_elev').val(elev);
+            }
+        }
+    });
+	
 	if (feature.attributes.verifiedLat){
 		$('#verified_lat').val(feature.attributes.verifiedLat);	
 	} else {
@@ -2467,14 +2434,15 @@ async function getElevation(dot, latlon, i, j, callback){
 	const apiKey = await getApiKey();
 
     $.ajax({
-		url: google_elevation_url+"/"+latlon.lat+'/'+latlon.lon,
+        url: elevation_url + latlon.lat + ',' + latlon.lon + '&key=' + apiKey,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        cache: false,
         success: function(result){
-			elev = result?.elevation;
-            if (elev == null || elev == undefined){
+            elev = result.resourceSets[0].resources[0].elevations[0];
+            if (elev == null){
                 elev = -9999; //any sea value is set to -9999 by default. This brings it back to sea level as we know it
-            }else{
-				elev = Math.round(elev);
-			}
+            }
             callback(elev, i, j, latlon, dot);
         },
         error: function(error){
